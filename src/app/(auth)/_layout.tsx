bf0e5d70544971159b322113f9a4ef93d3826f55
unknown <eiridere@gmail.com>
2025-09-29 18:40:03 +0300

    auth yapısı kuruldu

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,1 +1,16 @@
- 
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "../../providers/AuthProvider";
+export default function AuthLayout() {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated) {
+    return <Redirect href="/(protected)/" />;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="signin" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
